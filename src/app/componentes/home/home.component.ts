f7a85e8ca8906fd7248194ac9fcd3828b496426c
edugcravo <edugcravo@hotmail.com>
2024-07-02 01:10:20 -0300

    finalizado layout

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -8,12 +8,22 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   
+  resposta1: any = false;
+  resposta2: any = false;
+  resposta3: any = false;
+  resposta4: any = false;
+  resposta5: any = false;
 
   constructor() { }
   ngOnInit() {
    
   }
 
+  respostas: { [key: string]: boolean } = {
+    resposta1: false
+  };
 
- 
+  toggleResposta(resposta: string) {
+    this.respostas[resposta] = !this.respostas[resposta];
+  }
 }
