ef4507c37551a2f0eb26ae32f28621a5d521cea8
edugcravo <edugcravo@hotmail.com>
2024-07-05 01:28:18 -0300

    ajuste imagens

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -14,7 +16,50 @@ export class HomeComponent implements OnInit {
   resposta4: any = false;
   resposta5: any = false;
 
-  constructor() { }
+
+  nome: string = '';
+  numero: string = '';
+  email: string = '';
+  message: string = '';
+  isSuccess: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  enviar() {
+    const formData = new FormData();
+    formData.append('nome', this.nome);
+    formData.append('numero', this.numero);
+    formData.append('email', this.email);
+
+    this.http.post('https://formspree.io/f/xzzpbjow', formData, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).subscribe(
+      response => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Email enviado com sucesso!",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.isSuccess = true;
+        // Limpar os campos após o envio
+        this.nome = '';
+        this.numero = '';
+        this.email = '';
+      },
+      error => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Algo deu errado, tente novamente mais tarde!',
+        });
+        this.isSuccess = false;
+      }
+    );
+  }
   ngOnInit() {
    
   }
